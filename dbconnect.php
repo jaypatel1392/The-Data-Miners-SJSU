8dd0edc729422ccca3c8e2b18b4617b153124904
jaypatel1392 <jaypatel1392@gmail.com>
2014-11-13 22:47:56 -0800

    Db connect file.
    Try this. First of all make sure that you have ran the schema on
    workbench, (So the database is already created on the machine).

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+First make sure that our schema is already on the database. 
+**/
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "JAT Reservation System";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
