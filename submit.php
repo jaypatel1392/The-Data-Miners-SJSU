936cb43855049d7426cee5b5f2cbba6df38526a6
Aontiveros <antonioontiveros93@gmail.com>
2014-11-15 16:25:00 -0800

    Create submit.php
    Lets the user know if their item was successfully added to our management system, or if something went wrong.

---
+++ submit.php
@@ -0,0 +1,81 @@
+<?php
+include("dbconnect.php");
+?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Archivo+Narrow:400,700" rel="stylesheet" type="text/css">
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<!--[if IE 6]>
+<link href="default_ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+</head>
+<body>
+<div id="logo" class="container">
+	<h1><a href="#">JAT Hotels</a></h1>
+</div>
+<div id="menu-wrapper">
+	<div id="menu" class="container">
+		<ul>
+			<li><a href="#">Homepage</a></li>
+			<li class="current_page_item"><a href="rent.php">Rent a Room</a></li>
+			<li><a href="#">Manager Login</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Links</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+	</div>
+</div>
+<div id="page" class="container">
+	<div id="box1">
+		<h2 class="title"><a href="#">Welcome to JAT Hotels</a></h2>
+		<div style="clear: both;">&nbsp;</div>
+		<div class="entry">
+			<?php 
+				$error = false;
+				$hotel = $_POST['hotel'];
+				$room = $_POST['room'];
+				$eDate = $_POST['eDate'];
+				$sDate = $_POST['sDate'];
+				$name = mysqli_real_escape_string($conn,$_POST['name']);
+				$address = mysqli_real_escape_string($conn,$_POST['address']);
+				$credit = mysqli_real_escape_string($conn,$_POST['CC']);
+				$discount = mysqli_real_escape_string($conn,$_POST['discount']);
+				$smoke = mysqli_real_escape_string($conn,$_POST['smoke']);
+				if(empty($hotel) || empty($room) || empty($name) || empty($address) || empty($credit) || empty($discount) || empty($smoke))
+				{
+					$error = true;
+				}
+				if($error)
+					echo "<p>You must fill out the form! Please try to make a reservation again </p>";
+				else
+				{
+					$sql = "SELECT hID FROM hotels WHERE companyName = '$hotel'";
+					$result = $conn->query($sql);
+					$HID = $result->fetch_assoc();
+					$HID = $HID['hID'];
+					$sql = "SELECT count(*) AS number FROM customer";
+					$result = $conn->query($sql);
+					$counter = $result->fetch_assoc();
+					$counter = $counter['number'];
+					$query = "INSERT INTO customer VALUES( '$counter', '$HID', '$room', '$name', '$address', '$credit', '$smoke', '$sDate', '$eDate', '$discount');"; 
+					$result = mysqli_query($conn, $query);
+					if (!$result)
+					{
+						die('Invalid query: ' . mysqli_error($conn));
+					}
+					echo "<p>Successfully added your reservation for $hotel in " . $_POST['location'] . " room: $room on $sDate to $eDate</p>";
+				}
+					
+			?>	
+		</div>
+	</div>
+	
+</div>
+<div id="footer" class="container">
+	<p>&copy; Untitled. All rights reserved. Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. </p>
+</div>
+</body>
+</html>
