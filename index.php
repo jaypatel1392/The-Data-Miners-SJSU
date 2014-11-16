a8e39e54e36010e1ff56952f780f9676f5f76116
Aontiveros <antonioontiveros93@gmail.com>
2014-11-15 16:26:24 -0800

    Create index.php
    Our new index page, feel free to change it. I just changed the french text and the menu items uptop

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+Name       : Doubtless 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20130428
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Archivo+Narrow:400,700" rel="stylesheet" type="text/css">
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<!--[if IE 6]>
+<link href="default_ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+</head>
+<body>
+<div id="logo" class="container">
+	<h1><a href="#">JAT Hotels</a></h1>
+</div>
+<div id="menu-wrapper">
+	<div id="menu" class="container">
+		<ul>
+			<li class="current_page_item"><a href="#">Homepage</a></li>
+			<li><a href="rent.php">Rent a Room</a></li>
+			<li><a href="#">Manager Login</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Links</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+	</div>
+</div>
+<div id="page" class="container">
+	<div id="box1">
+		<h2 class="title"><a href="#">Welcome to JAT Hotels</a></h2>
+		<div style="clear: both;">&nbsp;</div>
+		<div class="entry">
+			<p>This is <strong>JAT Hotels </strong>. We hope that you find a room of your choosing in the multiple hotels that we offer.</p>
+		</div>
+	</div>
+	
+</div>
+<div id="footer" class="container">
+	<p>&copy; Untitled. All rights reserved. Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. </p>
+</div>
+</body>
+</html>
