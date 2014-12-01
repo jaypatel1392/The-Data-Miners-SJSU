24a85aad8e6e8101af9733682bc20619aa0b5a99
trannosaurus <vtran.sj@gmail.com>
2014-12-01 14:45:31 -0800

    updated to call new cancel proc

--- HotelReservationSystem/manager/cancel_reservation.php
+++ HotelReservationSystem/manager/cancel_reservation.php
@@ -46,7 +46,7 @@ include("../dbconnect.php") ?>
 		$managername = $_SESSION['managername'];
 		print "<h2 align='center'>$hotelname Manager Services</h2>"; 
 		print "<h3 align='center'>Welcome $managername</h3>";
-					
+		$n = null;
 		$sql = "SELECT * FROM customer where hID = $hID";
 		$result = mysqli_query($conn, $sql);
 		      
@@ -58,6 +58,7 @@ include("../dbconnect.php") ?>
             while(list($cID, $hid, $rID, $name, $address, $ccNo, $somker, $rStartDate, $rEndDate, $discount) = mysqli_fetch_array($result)) {
 				$info = $rStartDate . "," . $rEndDate . "," . $rID;
 				print "<option value='$info'>$name staying at Room #$rID</option>";
+				$n = $name;
 			}
 			print "</select></p>";
 			print "<p><input type=\"submit\" class=\"btn\"/></p>";
@@ -73,19 +74,25 @@ include("../dbconnect.php") ?>
 			$rEndDate = $in[1];
 			$rID = $in[2];
 			
+			$query = "select location from hotels where hID=$hID";
+			$city = $conn->query($query);
+			$city = $city->fetch_assoc();
+			$city = $city['location'];
+			
 			$conn = new PDO("mysql:=host=$servername;dbname=$dbname", $username, $password);
-			$stmt = $conn->prepare("CALL cancelReservation(:sDate, :eDate, :hotel, :roomID)");
+			$stmt = $conn->prepare("CALL cancelReservation(:sDate, :eDate, :hotel, :roomID, :city)");
 			
 			$stmt->bindParam(':sDate', $rStartDate, PDO::PARAM_STR);
 			$stmt->bindParam(':eDate', $rEndDate, PDO::PARAM_STR);
 			$stmt->bindParam(':hotel', $hotelname, PDO::PARAM_STR); 
-			$stmt->bindParam(':roomID', $rID, PDO::PARAM_INT); 
+			$stmt->bindParam(':roomID', $rID, PDO::PARAM_INT);
+			$stmt->bindParam(':city', $city, PDO::PARAM_STR);
 
 
 			// call the stored procedure
 			if ($stmt->execute()) {				
 				print "<span align='center'>";
-				print "<p><label class=\"formtext\">Successfully cancelled $name's reservation.</label></p>";
+				print "<p><label class=\"formtext\">Successfully cancelled $n's reservation.</label></p>";
 				print "<p align='center'><button onclick='window.location.href = \"home.php\" ' class=\"btn\" >Back to Home!</button></p>";
 				print "</span>";
 			}
